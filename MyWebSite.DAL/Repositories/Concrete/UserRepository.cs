6525eae62dc53a56377762c706bf10c446529815
Bugra Kırman <bugrakirman@gmail.com>
2019-02-06 09:02:40 +0300

    MVC_ADD

---
+++ MyWebSite.DAL/Repositories/Concrete/UserRepository.cs
@@ -0,0 +1,57 @@
+﻿using MyWebSite.DAL.MyEntities;
+using MyWebSite.DAL.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MyWebSite.DAL.Repositories.Concrete
+{
+    public class UserRepository : IUserRepository
+    {
+        MyWebSiteContext _db;
+        public UserRepository()
+        {
+            _db = new MyWebSiteContext();
+        }
+        public int AddItem(User item)
+        {
+            _db.User.Add(item);
+            int sonuc;
+            sonuc = _db.SaveChanges();
+            return sonuc;
+        }
+
+        public int DeleteItem(User item)
+        {
+            _db.User.Remove(item);
+            int sonuc;
+            sonuc = _db.SaveChanges();
+            return sonuc;
+        }
+
+        public ICollection<User> GetAllItem(Expression<Func<User, bool>> lambda = null)
+        {
+            return lambda == null ? _db.User.ToList() : _db.User.Where(lambda).ToList();
+        }
+
+        public User GetItem(Expression<Func<User, bool>> lambda = null)
+        {
+            return _db.User.Where(lambda).FirstOrDefault();
+        }
+
+        public int UpdateItem(User item)
+        {
+            User oldUser = _db.User.Where(x => x.UserId == item.UserId).FirstOrDefault();
+            oldUser.FirstName = item.FirstName;
+            oldUser.LastName = item.LastName;
+            oldUser.Gender = item.Gender;
+            oldUser.EMail = item.EMail;
+            oldUser.Password = item.Password;
+
+            int sonuc;
+            sonuc = _db.SaveChanges();
+            return sonuc;
+        }
+    }
+}
---
+++ MyWebSite/Models/UserVM.cs
@@ -0,0 +1,38 @@
+﻿using MyWebSite.DAL.MyEntities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MyWebSite.Models
+{
+    public class UserVM
+    {
+        public int UserId { get; set; }
+
+        [Required(ErrorMessage ="boş geçme")]
+        [MaxLength(20,ErrorMessage ="max 20 karakter olmalı")]
+
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "boş geçme")]
+        [MaxLength(20, ErrorMessage = "max 20 karakter olmalı")]
+
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "boş geçme")]
+        [MaxLength(50, ErrorMessage = "max 50 karakter olmalı")]
+        [EmailAddress(ErrorMessage ="mail formatında giriş yap")]
+
+        public string EMail { get; set; }
+
+        [Required(ErrorMessage = "boş geçme")]
+
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "boş geçme")]
+
+        public bool Gender { get; set; }
+    }
+}
